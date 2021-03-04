48b292dec02c02b0a1985042af5237b23c82a234
Carter Selman <46407649+cselman99@users.noreply.github.com>
2021-03-03 18:16:20 -0800

    Add files via upload

---
+++ ledger.py
@@ -0,0 +1,10 @@
+
+# TODO: Task III: Transaction File
+
+# ! Object format:
+# NUMBER: the hash of the input, output, and signature fields.
+# TYPE: either TRANS, JOIN, or MERGE
+# INPUT: a set of transaction numbers
+# OUTPUT: a set of (value:public key) pairs
+# SIGNATURE: a set of cryptographic signatures on the TYPE, INPUT, and OUTPUT fields
+
